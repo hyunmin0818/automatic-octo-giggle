13c739065b51288a8037398517bce78b43f03059
임현민 <120604k@gmail.com>
2024-02-23 09:12:58 +0900

    jsp db 1

--- jsp/jsp.sql
+++ jsp/jsp.sql
@@ -16,3 +16,26 @@ VALUES ('2','BMW', 'White', 3500000000);
 
 select sysdate from dual;
 
+CREATE TABLE MEMBER (
+	name varchar2(20),
+	age number(3)
+);
+
+SELECT * FROM MEMBER;
+
+INSERT INTO MEMBER VALUES ('김자바', 22);
+INSERT INTO MEMBER VALUES ('홍길동', 200);
+INSERT INTO MEMBER VALUES ('이순신', 460);
+
+SELECT * FROM MEMBER WHERE name='홍길동';
+
+CREATE TABLE tbl_member(
+	userid varchar2(200) PRIMARY KEY,
+	userpw varchar2(200),
+	username varchar2(200),
+	usergender varchar2(100),
+	userphone varchar2(100)
+	);
+	
+INSERT INTO tbl_member VALUES ('admin', 'admin1234', '홍길동', 'M', '01012345678');
+SELECT * FROM TBL_MEMBER ;
